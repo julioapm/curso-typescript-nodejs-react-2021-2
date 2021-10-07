d9b63e600e0266fb0435cf263769f39895eb6352
Júlio Pereira Machado <julioapm@gmail.com>
2021-10-07 17:31:02 -0300

    Código da moeda e cofre

---
+++ laboratorio3_exercicio2/src/index.ts
@@ -0,0 +1,32 @@
+class Moeda {
+    constructor(
+        private valor: number,
+        private nome: string
+    ) {}
+    get valorDaMoeda() {
+        return this.valor;
+    }
+    get nomeDaMoeda() {
+        return this.nome;
+    }
+}
+
+class Cofrinho {
+    private moedas: Moeda[] = [];
+    adicionar(moeda: Moeda) {
+        this.moedas.push(moeda);
+    }
+    calcularTotal() {
+        let total = 0;
+        this.moedas.forEach(moeda => {
+            total = total + moeda.valorDaMoeda;
+        });
+        return total;
+    }
+}
+
+let cofre = new Cofrinho();
+cofre.adicionar(new Moeda(1, 'Um Real'));
+cofre.adicionar(new Moeda(0.5, '50 centavos'));
+cofre.adicionar(new Moeda(0.25, '25 centavos'));
+console.log(cofre.calcularTotal());
