ca0d5bb4a0e92d14b1dc7df3d6dbc048e8836cdc
Júlio Pereira Machado <julioapm@gmail.com>
2021-10-13 17:13:32 -0300

    Código com callback

--- laboratorio4_exercicio1/src/index.ts
+++ laboratorio4_exercicio1/src/index.ts
@@ -1,14 +1,28 @@
 import { Cofrinho, Moeda } from './entidades';
-import { salvarCofre } from './persistencia';
+import { salvarCofre, lerCofre } from './persistencia';
 
+/*
 const cofre = new Cofrinho();
 cofre.adicionar(new Moeda(1,'Um Real'));
 cofre.adicionar(new Moeda(1,'Um Real'));
+cofre.adicionar(new Moeda(0.5,'50 Centavos'));
 console.log(cofre.calcularTotal());
-try {
-    salvarCofre(cofre, 'cofre.json');
-    //executar código aqui que depende do arquivo em disco, dará erro.
-} catch (error) {
-    console.log('Falha de escrita no arquivo:');
-    console.log(error);
-}
+salvarCofre(cofre, 'cofre.json', (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('Cofre salvo com sucesso!');
+    }
+});
+*/
+lerCofre('cofre.json', (err, cofre) => {
+    if (err) {
+        console.log('Falha de leitura');
+        console.log(err);
+    } else {
+        console.log('Leitura com sucesso');
+        if (cofre != null) {
+            console.log(cofre.calcularTotal());
+        }
+    }
+});
--- laboratorio4_exercicio1/src/persistencia.ts
+++ laboratorio4_exercicio1/src/persistencia.ts
@@ -1,17 +1,26 @@
-import { Cofrinho } from './entidades';
+import { Cofrinho, Moeda } from './entidades';
 import * as fs from 'fs';
 
-export function salvarCofre(cofre: Cofrinho, nomeArquivo: string) {
+export function salvarCofre(cofre: Cofrinho, nomeArquivo: string, callback: fs.NoParamCallback) {
     const json = JSON.stringify(cofre);
-    fs.writeFile(nomeArquivo, json, err => {
-        if (err) throw err;
-    });
+    fs.writeFile(nomeArquivo, json, callback);
 }
 
-export function lerCofre(nomeArquivo: string) {
+export function lerCofre(nomeArquivo: string,
+    callback: (err: Error | null, dados: Cofrinho | null) => void) {
     fs.readFile(nomeArquivo, 'utf8', (err, data) => {
-        if (err) throw err;
-        const cofre = JSON.parse(data);
-        return cofre;
+        if (err) {
+            callback(err, null);
+        }
+        try {
+            const obj = JSON.parse(data);
+            const cofre = new Cofrinho();
+            obj.moedas.forEach((m: any) => {
+                cofre.adicionar(new Moeda(m.valor, m.nome));
+            });
+            callback(null, cofre);
+        } catch (e:any) {
+            callback(e, null);
+        }
     });
 }
