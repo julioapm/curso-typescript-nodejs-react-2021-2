6ef7918ff953c623e75b9c0584fb5af2fbd2e450
Júlio Pereira Machado <julioapm@gmail.com>
2021-10-14 16:47:59 -0300

    Consultas básicas

--- typescriptnode_mongodb/src/index.ts
+++ typescriptnode_mongodb/src/index.ts
@@ -1,4 +1,5 @@
 import { MongoClient } from 'mongodb';
+import { Pessoa } from './pessoa';
 
 const uri = 'mongodb+srv://dbUser:kil,ji8oku@cluster0.wszic.mongodb.net/meubd?retryWrites=true&w=majority';
 const cliente = new MongoClient(uri);
@@ -7,6 +8,27 @@ async function main() {
     try {
         await cliente.connect();
         console.log('Conectado ao MongoDb Atlas');
+
+        const basededados = cliente.db('meubd');
+        const colecao = basededados.collection<Pessoa>('pessoas');
+    
+        /*
+        const pessoa: Pessoa = {
+            nome: 'João',
+            idade: 20
+        };
+        const resultado = await colecao.insertOne(pessoa);
+        console.log(`Inserido: ${resultado.insertedId}`);
+        */
+
+        const pessoas = await colecao.find().toArray();
+        console.log('Resultado da consulta:');
+        console.log(pessoas);
+
+        const numero = await colecao.find({idade: {$lte: 18}}).count();
+        console.log('Resultado da consulta:');
+        console.log(numero);
+
     } catch (error) {
         console.log('Falha de acesso ao BD:');
         console.error(error);
---
+++ typescriptnode_mongodb/src/pessoa.ts
@@ -0,0 +1,4 @@
+export interface Pessoa {
+    nome: string;
+    idade: number;
+}
