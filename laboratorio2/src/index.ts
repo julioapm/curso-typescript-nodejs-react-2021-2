49a189bd893ef3d377a7a6660fbb377ea61abb5e
fabiojcbatista <fabiojcbweb@gmail.com>
2021-10-16 09:23:09 -0300

    Contribuição - Laboratório2(Fabiojr)

--- laboratorio2/src/index.ts
+++ laboratorio2/src/index.ts
@@ -19,6 +19,24 @@ function imprimirParesFor() {
         }
     }
 }
+//FABIO JR//////
+let inicio:number = 0;
+let fim:number=10;
+let i = inicio;
+
+for(i=inicio;i<=fim;i++){
+    if(i % 2 == 0){
+        console.log(i+',');
+    }
+}
+
+while(inicio<=fim){
+    if(inicio % 2 == 0){
+        console.log(inicio+',');
+    }  
+    inicio++;
+}
+//FIM FABIOJR ///////////////
 
 //Exercício 2
 function loopInfinito() {
@@ -30,6 +48,11 @@ function loopInfinito() {
 //A condição i igual a 10 jamais será verdadeira, causando um loop infinito.
 //Isso se deve à representação de ponto flutuante para o tipo number possuir arredondamento.
 
+//FABIOJR
+//RESPOSTA -> Entra em estado de loop infinito
+console.log('Return 02 => Entra em estado de loop infinito');
+////////FIM
+
 //Exercício 3
 function min(x: number, y: number): number {
     if (x < y) {
@@ -38,6 +61,16 @@ function min(x: number, y: number): number {
         return y;
     }
 }
+
+//FABIOJR
+function min(x:number,y:number):number{
+    if (x<y){
+        return x;
+    }
+        return y;
+}
+console.log('Return 03 => '+min(12,10));
+/////FIM
 
 //Exercício 4
 function pow(x: number, y: number): number {
@@ -55,11 +88,28 @@ function powRecursivo(x: number, y: number): number {
         return x * powRecursivo(x, y - 1);
     }
 }
+//FABIO JR
+function pow (x:number,y:number):number{
+    if(y==0){
+        return 1;
+    }
+    return x*pow(x,y-1);
+}
+console.log('Return 04 => '+pow(2,3));
+//////////////FIM
 
 //Exercício 5
 function toMaiusculaPrimeira(s: string): string {
     return s.charAt(0).toUpperCase() + s.slice(1);
 }
+
+//FABIOJR
+const s:string = "treinaMENTO Dbserver";
+function toMaiusculaPrimeira(s:string):string{
+    return s.substring(0,1).toUpperCase()+s.substring(1,s.length).toLowerCase();
+}
+console.log('Return 05 => '+toMaiusculaPrimeira(s));
+////////////FIM
 
 //Exercício 6
 function getMax(array: number[]): number {
@@ -71,6 +121,21 @@ function getMax(array: number[]): number {
     }
     return maior;
 }
+
+//FABIOJR
+const arr:number[] = [1,2,3,4,5,6,7,8,900,10,20,30,40,50,60,70,80,90,100,200];
+function getMax(arr:number[]): number{
+    let maior:number = arr[0];
+    for(let i=0;i<=arr.length;i++){
+        if(arr[i]>maior){
+            maior=arr[i];
+        }
+    }
+    return maior;
+}
+
+console.log('Return 06 => '+getMax(arr));
+///////////////FIM
 
 //Exercício 7
 function frequencia(array: number[]): Map<number,number> {
