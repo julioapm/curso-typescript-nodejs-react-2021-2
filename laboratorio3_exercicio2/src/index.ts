6dab080f255c6ab58af08d53b7ad0bf720c00711
Nicolás Gonçalves Batista da Silva <nicolasgbdas@gmail.com>
2021-10-19 10:13:14 -0300

    lab3ex2 completo

--- laboratorio3_exercicio2/src/index.ts
+++ laboratorio3_exercicio2/src/index.ts
@@ -11,17 +11,22 @@ class Moeda {
     }
 }
 
-class Cofrinho{
-     private moedas: Moeda[] = [];
-     adicionar(moeda: Moeda){
-         this.moedas.push(moeda);
-     }
-     calcTotal(){
-         let total = 0;
-         this.moedas.forEach(moeda => {
-             total = total + moeda.valorMoeda;
-         });
-         return total;
-     }
-     
+class Cofrinho {
+    private moedas: Moeda[] = [];
+    adicionar(moeda: Moeda) {
+        this.moedas.push(moeda);
+    }
+    calcTotal() {
+        let total = 0;
+        this.moedas.forEach(moeda => {
+            total = total + moeda.valorMoeda;
+        });
+        return total;
+    }
 }
+
+let litleCofre = new Cofrinho();
+litleCofre.adicionar(new Moeda(0.25, "Vinte e Cinco Centavos"));
+litleCofre.adicionar(new Moeda(0.50, "Cinquenta Centavos"));
+litleCofre.adicionar(new Moeda(1, "Um Real"));
+console.log(litleCofre.calcTotal());
