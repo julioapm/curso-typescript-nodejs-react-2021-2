1bc950afe7d3cee113d0d3eb3d46d4d905b0dce3
Nicolás Gonçalves Batista da Silva <nicolasgbdas@gmail.com>
2021-10-19 10:02:08 -0300

    lab3

--- laboratorio3_exercicio2/src/index.ts
+++ laboratorio3_exercicio2/src/index.ts
@@ -1,65 +1,27 @@
-class moeda {
+class Moeda {
     constructor(
         private valorDaMoeda: number,
         private nomeDaMoeda: string
     ) { }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*class Moeda {
-    constructor(
-        private valor: number,
-        private nome: string
-    ) {}
-    get valorDaMoeda() {
-        return this.valor;
+    get valorMoeda() {
+        return this.valorDaMoeda
     }
-    get nomeDaMoeda() {
-        return this.nome;
+    get nomeMoeda() {
+        return this.nomeDaMoeda
     }
 }
 
-class Cofrinho {
-    private moedas: Moeda[] = [];
-    adicionar(moeda: Moeda) {
-        this.moedas.push(moeda);
-    }
-    calcularTotal() {
-        let total = 0;
-        this.moedas.forEach(moeda => {
-            total = total + moeda.valorDaMoeda;
-        });
-        return total;
-    }
+class Cofrinho{
+     private moedas: Moeda[] = [];
+     adicionar(moeda: Moeda){
+         this.moedas.push(moeda);
+     }
+     calcTotal(){
+         let total = 0;
+         this.moedas.forEach(moeda => {
+             total = total + moeda.valorMoeda;
+         });
+         return total;
+     }
+     
 }
-
-let cofre = new Cofrinho();
-cofre.adicionar(new Moeda(1, 'Um Real'));
-cofre.adicionar(new Moeda(0.5, '50 centavos'));
-cofre.adicionar(new Moeda(0.25, '25 centavos'));
-console.log(cofre.calcularTotal());
-console.log(JSON.stringify(cofre));*/
