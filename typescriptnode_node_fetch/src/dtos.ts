13ede667b90db66ca684a58a8f1ace11f882d54e
LizSantana <liz.santanabrito@gmail.com>
2021-10-20 18:17:17 -0300

    Método post e put mesma estrutura, put passa o id

---
+++ typescriptnode_node_fetch/src/dtos.ts
@@ -0,0 +1,7 @@
+export interface PostDTO {
+    userId: number;
+    id?: number;
+    title: string;
+    body: string;
+
+}
--- typescriptnode_node_fetch/src/index.ts
+++ typescriptnode_node_fetch/src/index.ts
@@ -1,26 +1,52 @@
 import fetch from 'node-fetch';
+import {PostDTO} from './dtos'
 
 async function main() {
 
     const uriBase = 'https://jsonplaceholder.typicode.com';
     try {
-        const resposta = await fetch(`${uriBase}/posts`);
-        if (resposta.ok) {
+        //let id = 1;
+        //const resposta = await fetch(`${uriBase}/posts/${id}`);
+     /*   if (resposta.ok) {
            // const dadostexto = await resposta.text();
-            const dadosjson = await resposta.json();
+            const dadosjson: PostDTO = await resposta.json();
             console.log('Dados: ');
-            console.log(dadosjson);
+            console.log(dadosjson.title);
             //console.log(dadosjson.length);
             
         } else {
             console.log('GET status: ', resposta.status);
             console.log('GET statusText: ', resposta.statusText);
+        }*/
+        //Realizar um POST
+        let post: PostDTO = {
+            id: 1,
+            userId: 1,
+            title: "Bechamel",
+            body: "Tutorial no Youtube de Paola Carosella"
+        };
+        const resposta = await fetch(`${uriBase}/posts/${post.id}`, {
+            method: 'PUT',
+            headers:{
+                'Content-Type': 'application/json',
+            }, 
+            body: JSON.stringify(post)
+        });
+         if (resposta.ok) {
+            const dadosjson: PostDTO = await resposta.json();
+            console.log('Dados: ');
+            console.log(dadosjson);
+            
+        } else {
+            console.log('PUT status: ', resposta.status);
+            console.log('PUT statusText: ', resposta.statusText);
         }
         
     } catch (error) {
         console.log('Falha no acesso ao wen service: ')
         console.log(error);
     }
+
 
 }
 
