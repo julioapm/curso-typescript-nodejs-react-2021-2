70b9fb7ef89750fac3f1a7d8e077f9442866a599
Júlio Pereira Machado <julioapm@gmail.com>
2021-10-21 15:25:04 -0300

    Middleware de logging

--- typescriptnode_express_alomundo/src/index.ts
+++ typescriptnode_express_alomundo/src/index.ts
@@ -1,9 +1,10 @@
-import express from 'express';
+import express, {Request, Response, NextFunction} from 'express';
 import { json } from 'body-parser';
 
 const app = express();
 const port = 3000;
 
+app.use(consoleLogger);
 app.use(json());
 
 app.get('/', (req, res) => {
@@ -17,9 +18,23 @@ app.get('/alo/:nome', (req, res) => {
 
 app.post('/alo', (req, res) => {
     const {nome} = req.body;
-    res.send(`Alô, ${nome}!`);
+    if (nome) {
+        res.send(`Alô, ${nome}!`);
+    } else {
+        res.status(400).send('Nome não informado');
+    }
+});
+
+app.get('/erro', (req, res) => {
+    throw new Error('Erro inesperado');
 });
 
 const server = app.listen(port, () => {
     console.log(`Express na porta ${port}`);
 });
+
+//função de middleware
+function consoleLogger(req: Request, res: Response, next: NextFunction) {
+    console.log(`${req.method} ${req.path}`);
+    next();
+}
