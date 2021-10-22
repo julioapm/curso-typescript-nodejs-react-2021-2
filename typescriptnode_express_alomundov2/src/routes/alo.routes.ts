aaa10869af7fe75926ad76af05bc841abc7579fe
Júlio Pereira Machado <julioapm@gmail.com>
2021-10-22 15:21:27 -0300

    Configuração express-validator

--- typescriptnode_express_alomundov2/src/routes/alo.routes.ts
+++ typescriptnode_express_alomundov2/src/routes/alo.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { body } from 'express-validator';
 import * as AloController from '../controllers/alo.controller';
 
 export const router = Router();
@@ -7,3 +8,6 @@ router.get(path, AloController.getAlo);
 router.get(`${path}/erro`, AloController.getAloErro);
 router.get(`${path}/:nome`, AloController.getAloComParametro);
 router.post(path, AloController.postAlo);
+router.post(`${path}/validado`,
+            body('nome').notEmpty().trim().isLength({min:1, max:25}).withMessage('Nome deve ter entre 1 e 25 caracteres'),
+            AloController.postAloValidado);
