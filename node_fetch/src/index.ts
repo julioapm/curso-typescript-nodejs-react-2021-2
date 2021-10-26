a92e559fa1ac86cbe4cc27443b73f16470714e8b
Your Name <nickk.lima17@gmail.com>
2021-10-26 14:40:07 -0300

    node-fetch cont

--- node_fetch/src/index.ts
+++ node_fetch/src/index.ts
@@ -1,10 +1,21 @@
-import {} from 'node-fetch'
-
+import fetch from 'node-fetch'
+ 
 async function main() {
-    const uriBase = 'https://postman-echo.com';
+    const uriBase = 'https://jsonplaceholder.typicode.com';
     try {
-        
+        const resposta = await fetch(`${uriBase}/posts`);
+        if (resposta.ok) {
+            const dadostext = await resposta.json();
+            console.log('Dados:');
+            console.log(dadostext);
+        } else {
+            console.log('GET status:', resposta.status);
+            console.log('GET statusText:', resposta.statusText);
+        }
     } catch (error) {
-        
+        console.log('Falha no acesso ao web service');
+        console.log(error);
     }
 }
+
+main();
