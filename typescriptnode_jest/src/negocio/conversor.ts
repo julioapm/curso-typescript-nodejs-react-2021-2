ccb6f718f6ef95874e03095a4c9df3669a37e53b
fabiojcbatista <fabiojcbweb@gmail.com>
2021-10-27 14:12:10 -0300

    atualização 2710

--- typescriptnode_jest/src/negocio/conversor.ts
+++ typescriptnode_jest/src/negocio/conversor.ts
@@ -3,6 +3,13 @@ import { MoedaRepositorio } from '../persistencia/moedaRepositorio';
 
 export class Conversor {
     static async converter(codigoMoeda: string, quantidade: number): Promise<number> {
-        ???
+        const moeda = await MoedaRepositorio.buscarPorCodigo(codigoMoeda);
+        if (moeda) {
+            return quantidade * moeda.cotacao;
+        }
+        throw new Error('Código de moeda inexistente');
+    }
+    static async listarMoedas(): Promise<Moeda[]> {
+        return await MoedaRepositorio.buscar();
     }
 }
