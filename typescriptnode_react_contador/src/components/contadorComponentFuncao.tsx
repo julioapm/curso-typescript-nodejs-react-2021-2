35b09f68625e5a628623fd0c75a48f65254e5880
Júlio Pereira Machado <julioapm@gmail.com>
2021-10-29 16:36:05 -0300

    código componente funcional

---
+++ typescriptnode_react_contador/src/components/contadorComponentFuncao.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+
+interface Props {
+    valorInicial: number;
+}
+interface State {
+    contagem: number;
+}
+
+const ContadorFuncao = (props: Props) => {
+    const [state, setState] = useState<State>({contagem: props.valorInicial});
+    return (
+        <div>
+            <h2>{state.contagem}</h2>
+            <button onClick={() => { setState({contagem: state.contagem + 1}) }}>Incrementar</button>
+        </div>
+    );
+};
+
+export default ContadorFuncao;
