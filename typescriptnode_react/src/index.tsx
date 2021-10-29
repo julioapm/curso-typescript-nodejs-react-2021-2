70b1e1c2ab67b0ef8b8c3ddf013b195495cf047e
Júlio Pereira Machado <julioapm@gmail.com>
2021-10-29 13:18:41 -0300

    Ajuste import

--- typescriptnode_react/src/index.tsx
+++ typescriptnode_react/src/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 import { AloComponenteFuncao } from './aloComponenteFuncao';
 import { AloComponenteClasse } from './aloComponenteClasse';
 
