442af8676493fc3499a120af611529030a2d712d
Júlio Pereira Machado <julioapm@gmail.com>
2021-10-29 17:26:50 -0300

    código relógio componente classe

---
+++ typescriptnode_react_relogio/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import RelogioClasse from './components/relogioComponentClasse';
+
+function App() {
+  return (
+    <div>
+      <RelogioClasse />
+    </div>
+  );
+}
+
+export default App;
---
+++ typescriptnode_react_relogio/src/components/relogioComponentClasse.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+
+interface Props {}
+interface State {
+    data: Date;
+}
+
+export default class RelogioClasse extends React.Component<Props,State> {
+
+    constructor(props: Props, private timerID: NodeJS.Timeout) {
+        super(props);
+        this.state = {
+            data: new Date()
+        };
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Relógio:</h1>
+                <h2>{this.state.data.toLocaleTimeString()}</h2>
+            </div>
+        );
+    }
+
+    tick() {
+        this.setState({
+            data: new Date()
+        });
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(() => this.tick(), 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+}
