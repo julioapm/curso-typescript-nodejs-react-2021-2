0472d0832806f7bba7c46b2223c5eaf6d6c59079
fabiojcbatista <fabiojcbweb@gmail.com>
2021-11-04 16:11:12 -0300

    Codigo inicio viaCEP

--- typescriptnode_react_viacep/src/App.tsx
+++ typescriptnode_react_viacep/src/App.tsx
@@ -1,25 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState, useEffect} from 'react';
 import './App.css';
+import {Cep} from './dtos';
 
 function App() {
+  const [dados, setDados] = useState<Cep>();
+  const [cep, setCep] = useState('94360100');
+  const [url, setUrl] = useState('https://viacep.com.br/ws/94360100/json/');
+  const [carregando, setCarregando] = useState(false);
+  const [erro, setErro] = useState(false);
+
+  useEffect(()=> {},[url]);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit = {event=> {
+        setUrl(`viacep.com.br/ws/${cep}/json/`);
+        event.preventDefault();
+      }}>
+        <fieldset>
+          <legend>Buscar ViaCEP</legend>
+          <input type= "text" value={cep} onChange={event=>setCep(event.target.value)}/>
+          <button type="submit"> Buscar</button>
+        </fieldset>
+      </form>
+        {erro && <div>Ocorreu um erro!</div>}
+        {carregando ? (<div>Carregando...</div>):(
+          dados && (
+            <div>
+              <p>CEP: {dados.cep}</p>
+              <p>Logradouro: {dados.logradouro}</p>
+              <p>Complementos: {dados.complemento}</p>
+              <p>Bairro: {dados.bairro}</p>
+              <p>Localidade: {dados.localidade}</p>
+              <p>UF: {dados.uf}</p>
+            </div>
+          )
+        )}
+      
+    </>
+
+    //Resultado
   );
 }
 
