2285c7b96c67f4a5bb827370f3839132b31631fb
fabiojcbatista <fabiojcbweb@gmail.com>
2021-11-04 17:44:50 -0300

    Busca ViaCEP

--- typescriptnode_react_viacep/src/App.tsx
+++ typescriptnode_react_viacep/src/App.tsx
@@ -1,45 +1,62 @@
-import React,{useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import {Cep} from './dtos';
+import { Cep } from './dtos';
 
 function App() {
   const [dados, setDados] = useState<Cep>();
-  const [cep, setCep] = useState('94360100');
-  const [url, setUrl] = useState('https://viacep.com.br/ws/94360100/json/');
+  const [cep, setCep] = useState('90000000');
+  const [url, setUrl] = useState('https://viacep.com.br/ws/90000000/json/');
   const [carregando, setCarregando] = useState(false);
   const [erro, setErro] = useState(false);
 
-  useEffect(()=> {},[url]);
-  
+  useEffect(() => {
+    async function consultarViaCep() {
+      setErro(false);
+      setCarregando(true);
+      try {
+        const resultado = await fetch(url);
+        if (resultado.ok) {
+          const dados: Cep = await resultado.json();
+          setDados(dados);
+        } else {
+          setErro(true);
+        }
+      } catch (error) {
+        setErro(true);
+      }
+      setCarregando(false);
+    }
+    consultarViaCep();
+  }, [url]);
+
   return (
     <>
-      <form onSubmit = {event=> {
-        setUrl(`viacep.com.br/ws/${cep}/json/`);
+      <form onSubmit={event => {
+        setUrl(`https://viacep.com.br/ws/${cep}/json/`);
         event.preventDefault();
       }}>
         <fieldset>
-          <legend>Buscar ViaCEP</legend>
-          <input type= "text" value={cep} onChange={event=>setCep(event.target.value)}/>
-          <button type="submit"> Buscar</button>
+          <legend>Busca ViaCEP</legend>
+          <input type="text" value={cep} onChange={event => setCep(event.target.value)}/>
+          <button type="submit">Buscar</button>
         </fieldset>
       </form>
-        {erro && <div>Ocorreu um erro!</div>}
-        {carregando ? (<div>Carregando...</div>):(
-          dados && (
-            <div>
-              <p>CEP: {dados.cep}</p>
-              <p>Logradouro: {dados.logradouro}</p>
-              <p>Complementos: {dados.complemento}</p>
-              <p>Bairro: {dados.bairro}</p>
-              <p>Localidade: {dados.localidade}</p>
-              <p>UF: {dados.uf}</p>
-            </div>
-          )
-        )}
-      
+      {erro && <div>Ocorreu um erro!</div>}
+      {carregando ? (
+        <div>Carregando...</div>
+      ) : (
+        dados && (
+          <div>
+            <p>CEP: {dados.cep}</p>
+            <p>Logradouro: {dados.logradouro}</p>
+            <p>Complemento: {dados.complemento}</p>
+            <p>Bairro: {dados.bairro}</p>
+            <p>Localidade: {dados.localidade}</p>
+            <p>UF: {dados.uf}</p>
+          </div>
+        )
+      )}
     </>
-
-    //Resultado
   );
 }
 
