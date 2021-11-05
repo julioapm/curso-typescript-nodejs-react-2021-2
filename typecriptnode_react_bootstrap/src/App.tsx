99bd007deec85b68fb70ac70e145747246547295
Júlio Pereira Machado <julioapm@gmail.com>
2021-11-05 17:48:16 -0300

    Código base com navbar

---
+++ typecriptnode_react_bootstrap/src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Navbar, Nav } from 'react-bootstrap';
+import { NavLink, Outlet } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand as={NavLink} to="/">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={NavLink} to="/Home">Home</Nav.Link>
+            <Nav.Link as={NavLink} to="/sobre">Sobre</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Outlet />
+    </div>
+  );
+}
+
+export default App;
---
+++ typecriptnode_react_bootstrap/src/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Home';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} >
+        <Route path="home" element={<Home />} />
+        <Route path="sobre" element={<Home />} />
+      </Route>
+    </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
