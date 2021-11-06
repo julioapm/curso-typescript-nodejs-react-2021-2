6be07094c5f73bf219d20f7489b87a1018b6bcaa
fabiojcbatista <fabiojcbweb@gmail.com>
2021-11-06 09:06:21 -0300

    React_Router

---
+++ typescriptnode_react_router/src/PaginaUsuarios.tsx
@@ -0,0 +1,44 @@
+import { Link, Outlet, useSearchParams } from 'react-router-dom';
+import { getUsuarios } from './dados';
+
+export default function PaginaUsuarios() {
+    let usuarios = getUsuarios();
+    let [searchParams, setSearchParams] = useSearchParams();
+    return (
+        <>
+            <input
+                type="search"
+                value={searchParams.get('filtro') || ''}
+                onChange={event => {
+                    let filtro = event.target.value;
+                    if (filtro) {
+                        setSearchParams({ filtro });
+                    } else {
+                        setSearchParams({});
+                    }
+                }}
+            />
+            <nav>
+                {usuarios
+                .filter(usuario => {
+                    let filtro = searchParams.get('filtro');
+                    if (!filtro) {
+                        return true;
+                    }
+                    let nome = usuario.nome.toLowerCase();
+                    return nome.startsWith(filtro.toLowerCase());
+                })
+                .map(usuario => (
+                    <Link
+                        key={usuario.id}
+                        to={`/usuarios/${usuario.id}`}
+                        style={{ display: 'block', margin: '1rem 0' }}
+                    >
+                        {usuario.nome}
+                    </Link>
+                ))}
+            </nav>
+            <Outlet />
+        </>
+    );
+}
