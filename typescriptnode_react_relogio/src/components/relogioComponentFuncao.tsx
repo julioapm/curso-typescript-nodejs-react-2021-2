35d4157c1b12b9aaafda66f9f6e75e02d459b9d7
Júlio Pereira Machado <julioapm@gmail.com>
2021-12-28 13:40:35 -0300

    Atualização

--- typescriptnode_react_relogio/src/components/relogioComponentFuncao.tsx
+++ typescriptnode_react_relogio/src/components/relogioComponentFuncao.tsx
@@ -1,5 +1,4 @@
-
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const RelogioFuncao = () => {
     const [data, setData] = useState(new Date());
